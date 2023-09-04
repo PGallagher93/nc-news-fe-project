6317154c39b1454bd940ca1d91ee416e375cdd9f
PGallagher93 <Pgallagher93@hotmail.com>
2023-09-04 16:05:21 +0100

    adds link to articles from nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,11 @@
-
+import {Link} from 'react-router-dom'
 const NavBar = () =>{
     return (
         <ul>
             <li>Home</li>
+            <Link to="/articles">
             <li>News</li>
+            </Link>
             <li>Topics</li>
         </ul>
     )
