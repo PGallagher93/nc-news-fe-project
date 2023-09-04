f0f3b1623833d82fa2405a2df5c08452ddea8192
PGallagher93 <Pgallagher93@hotmail.com>
2023-09-04 14:25:10 +0100

    adds header and nav components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar"
+const Header = () =>{
+    return (
+        <header>
+            <NavBar />
+            <h3>NC NEWS</h3>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+
+const NavBar = () =>{
+    return (
+        <ul>
+            <li>Home</li>
+            <li>News</li>
+            <li>Topics</li>
+        </ul>
+    )
+}
+
+export default NavBar
