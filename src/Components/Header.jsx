8f52871e7bb0affef2b4369bea90bc2ec71b81aa
PGallagher93 <Pgallagher93@hotmail.com>
2023-11-06 14:22:14 +0000

    adds base structure for MUI navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,9 @@
 import NavBar from "./NavBar"
+
 const Header = () =>{
     return (
         <header>
-            <h3>NC NEWS</h3>
+            
             <NavBar />
             
         </header>
