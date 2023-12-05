1150ccfaea8ca32d7625071cb74ffc09f49e2b7d
PGallagher93 <Pgallagher93@hotmail.com>
2023-12-05 15:55:51 +0000

    adds imports for mui card component

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,8 +1,15 @@
 import {Link} from 'react-router-dom'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
 
 const ArticleCard = ({article}) => {
     return (
     <li className = "article-card">
+        <Card>
+            hi
+        </Card>
         <p>{article.title}</p>
         <Link to={`/articles/${article.article_id}`}>
         <img className="article-thumbnail" src={article.article_img_url} alt={article.title}/>
