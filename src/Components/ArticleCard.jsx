651f8a5523076c69536c543b38e3b1493d7ed194
PGallagher93 <Pgallagher93@hotmail.com>
2023-12-31 18:25:22 +0000

    adds icons to article cards

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -4,28 +4,39 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import CommentIcon from '@mui/icons-material/Comment';
 const ArticleCard = ({article}) => {
-    console.log(article.article_img_url)
+    console.log(article)
     return (
     <li className = "article-card">
-        <Card>
+        <Link to={`/articles/${article.article_id}`}>
+        <Card sx={{ width: 345, 
+                    margin: 2}}>
             <CardMedia
                 component="img"
                 src={article.article_img_url}
-                height="140"
+                height='140'
+                
+                
             />
-            <CardContent>
+            <CardContent
+            width="400">
                 <Typography variant='h6'>
                     {article.title}
                 </Typography>
             </CardContent>
+            <cardContent>
+            <FavoriteIcon sx={{marginRight:1}}/>
+                <strong>{`${article.votes}`}</strong>
+                <CommentIcon sx={{maringRight:1}}/>
+                <strong>{`${article.comment_count}`}</strong>
+            </cardContent>
         
         </Card>
-        <p>{article.title}</p>
-        <Link to={`/articles/${article.article_id}`}>
-        <img className="article-thumbnail" src={article.article_img_url} alt={article.title}/>
-        </Link >
-        <p>{article.author}</p>
+        </Link>
+        
+        
     </li>
     )
 }
