163636c283cb9f63e6952ff80cfee65a1a83fac8
PGallagher93 <Pgallagher93@hotmail.com>
2024-01-16 22:14:55 +0000

    adds fetch users to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,21 @@
+import { Box, Typography } from "@mui/material"
+import { fetchUsers} from "../api"
+import {useEffect, useState} from "react"
+
 const LoginPage = () => {
-    
+    const [allUsers, setAllUsers] = useState([])
+    useEffect(()=>{
+        fetchUsers()
+          .then((res)=>{
+            setAllUsers(res.data.users)
+          })
+    }, [])
+
+    console.log(allUsers)
     return (
-    <p>placeholder</p>
+    <Box>
+        <Typography>Select your user</Typography>
+    </Box>
 )}
 
 export default LoginPage
