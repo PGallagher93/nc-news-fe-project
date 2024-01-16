b4035b9ab58d9c03f002d495cbde541fda8d92ec
PGallagher93 <Pgallagher93@hotmail.com>
2024-01-16 22:48:22 +0000

    adds user info list items to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material"
+import { Box, ImageList, ImageListItem, ImageListItemBar, ListItemButton, Typography } from "@mui/material"
 import { fetchUsers} from "../api"
 import {useEffect, useState} from "react"
 
@@ -13,8 +13,32 @@ const LoginPage = () => {
 
     console.log(allUsers)
     return (
-    <Box>
-        <Typography>Select your user</Typography>
+    <Box sx={{pt:'4rem'}}>
+        <Typography variant="h3" align="center">Select your user</Typography>
+        <ImageList cols={2} sx={{px:4}}>
+            {allUsers.map((user) => {
+                return (
+                <ImageListItem  key={user.username}>
+                    <ListItemButton sx={{maxHeight: {xs: '10rem', md: '20rem'}}} >
+                    <img 
+                      srcSet={user.avatar_url}
+                      alt={user.username}
+                      style={{
+                        width: "100%",
+                        height:"100%",
+                        objectFit: "contain",
+                        margin: "auto",
+                      }}/>
+                      <ImageListItemBar
+                    title={user.username}
+                    subtitle={user.name}/>
+                    </ListItemButton>
+                </ImageListItem>
+                
+                )
+            })}
+
+        </ImageList>
     </Box>
 )}
 
