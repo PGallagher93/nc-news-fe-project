82b34c5b79d0e353305dc3a8836a5ad74bc019fd
PGallagher93 <Pgallagher93@hotmail.com>
2024-01-23 18:23:40 +0000

    adds loading component to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,87 +1,113 @@
-import { ImageList, ImageListItem, useMediaQuery, ImageListItemBar, ListItemButton, Typography } from "@mui/material"
-import{useEffect} from 'react'
-import HomeArticleContainer from "../Components/HomeArticleContainer"
-import Box from '@mui/system/Box'
-import TopicBar from "../Components/TopicBar"
-import { Link } from 'react-router-dom';
-const Homepage =({articles, isLoading, setArticleQuery}) =>{
-const matches = useMediaQuery('(min-width:600px)')
-   useEffect(()=>{
-    setArticleQuery((currVal) =>{
-        return {...currVal, topic:null}
+import {
+  ImageList,
+  ImageListItem,
+  useMediaQuery,
+  ImageListItemBar,
+  ListItemButton,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import { useEffect } from "react";
+import HomeArticleContainer from "../Components/HomeArticleContainer";
+import Box from "@mui/system/Box";
+import TopicBar from "../Components/TopicBar";
+import { Link } from "react-router-dom";
+const Homepage = ({ articles, isLoading, setArticleQuery }) => {
+  const matches = useMediaQuery("(min-width:600px)");
+  useEffect(() => {
+    setArticleQuery((currVal) => {
+      return { ...currVal, topic: null };
+    });
+  }, []);
+  
+  const articlesToDisplay = articles.slice(0, 6);
+
+  const mostPopularArticles = articles
+    .sort((a, b) => {
+      return b.votes - a.votes;
     })
-   }, [])
-   
-    const articlesToDisplay = articles.slice(0, 6)
-    
+    .slice(0, 6);
+
     if(isLoading){
-        return (
-            <p>Loading...</p>
+        return (<Box sx={{pl:'45%', pt:"20rem"}}>
+            <CircularProgress/>
+            </Box>
         )
     }
-    
-    const mostPopularArticles = articles.sort((a, b) => { return b.votes - a.votes}).slice(0,6)
-   
-    return (
-        <Box sx={{pt:'4rem',
-                  display:'flex',
-                  flexDirection:'column',
-                  justifyContent:'center',
-                  
-                  }}>
-        <Typography 
-        variant="h3"
-        align="left"
-        sx={{pl:{xs:'1rem'}}}
-        >Latest stories</Typography>
-        <ImageList cols={matches ? 3 : 1} >
-            {articlesToDisplay.map((article) =>{
-                return (
-                    <ImageListItem key={article.title} sx={{padding:{xs:'1rem', md:'0'}}}>
-                        <ListItemButton component={Link} to={`/articles/${article.article_id}`} sx={{padding: 0, height:'25vh'}}>
-                            <img 
-                             srcSet={article.article_img_url}
-                             style={{
-                                width:'100%',
-                                height:'100%',
-                               
-                             }}/>
-                             <ImageListItemBar
-                              title={article.title}
-                              subtitle={article.author}/>
-                        </ListItemButton>
-                    </ImageListItem>
-                )
-                
-            })}
-        </ImageList>
-        <Typography
-        variant='h3'
-        alight='left'
-        sx={{pl:{xs:'1rem'}}}>Popular stories</Typography>
-        <ImageList cols={matches ? 3 : 1} >
-            {mostPopularArticles.map((article) =>{
-                return (
-                    <ImageListItem key={article.title} sx={{padding:{xs:'1rem', md:'0'}}} >
-                        <ListItemButton component={Link} to={`/articles/${article.article_id}`} sx={{padding: 0, height:'25vh'}}>
-                            <img 
-                             srcSet={article.article_img_url}
-                             style={{
-                                width:'100%',
-                                height:'100%',
-                                
-                             }}/>
-                             <ImageListItemBar
-                              title={article.title}
-                              subtitle={article.author}/>
-                        </ListItemButton>
-                    </ImageListItem>
-                )
-                
-            })}
-        </ImageList>
-        </Box>
-    )
-}
 
-export default Homepage
+  return (
+    <Box
+      sx={{
+        pt: "4rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="h3" align="left" sx={{ pl: { xs: "1rem" } }}>
+        Latest stories
+      </Typography>
+      <ImageList cols={matches ? 3 : 1}>
+        {articlesToDisplay.map((article) => {
+          return (
+            <ImageListItem
+              key={article.title}
+              sx={{ padding: { xs: "1rem", md: "0" } }}
+            >
+              <ListItemButton
+                component={Link}
+                to={`/articles/${article.article_id}`}
+                sx={{ padding: 0, height: "25vh" }}
+              >
+                <img
+                  srcSet={article.article_img_url}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+                <ImageListItemBar
+                  title={article.title}
+                  subtitle={article.author}
+                />
+              </ListItemButton>
+            </ImageListItem>
+          );
+        })}
+      </ImageList>
+      <Typography variant="h3" alight="left" sx={{ pl: { xs: "1rem" } }}>
+        Popular stories
+      </Typography>
+      <ImageList cols={matches ? 3 : 1}>
+        {mostPopularArticles.map((article) => {
+          return (
+            <ImageListItem
+              key={article.title}
+              sx={{ padding: { xs: "1rem", md: "0" } }}
+            >
+              <ListItemButton
+                component={Link}
+                to={`/articles/${article.article_id}`}
+                sx={{ padding: 0, height: "25vh" }}
+              >
+                <img
+                  srcSet={article.article_img_url}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+                <ImageListItemBar
+                  title={article.title}
+                  subtitle={article.author}
+                />
+              </ListItemButton>
+            </ImageListItem>
+          );
+        })}
+      </ImageList>
+    </Box>
+  );
+};
+
+export default Homepage;
